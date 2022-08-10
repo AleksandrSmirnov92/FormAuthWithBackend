46b020a498a549d6f7043cde7ba105ce3f3256c3
AleksandrSmirnov92 <ryan00@mail.ru>
2022-08-10 21:22:32 +0300

    Делаю форму

--- public/src/clietsApp.js
+++ public/src/clietsApp.js
@@ -3,6 +3,8 @@ const button_Sign_in = document.querySelector(".button_Sign_in");
 const button_Sign_up = document.querySelector(".button_Sign_up");
 const FormSignIn = document.querySelector(".wrapper_form_Sign_in");
 const FormSignUp = document.querySelector(".wrapper_form_Sign_up");
+const Button_submit_SignUp = document.querySelector("#Form1");
+const Button_submit_SignIn = document.querySelector("#Form2");
 let state = {
     classButton: "",
 };
@@ -15,6 +17,16 @@ button_Sign_in.addEventListener("click", () => {
     state.classButton = "Sign_in";
     console.log("Войти", state.classButton);
     changeSignUpAndSignIn();
+});
+Button_submit_SignUp.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let form = document.querySelector("form");
+    console.log(form.element);
+    console.log("форма отправилась1");
+});
+Button_submit_SignIn.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("форма отправилась2");
 });
 function changeSignUpAndSignIn() {
     if (state.classButton === "Sign_up") {
