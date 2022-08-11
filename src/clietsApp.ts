1ebe074532baafd2890fcb9962beec527f7b7b1f
AleksandrSmirnov92 <ryan00@mail.ru>
2022-08-11 17:44:22 +0300

    front

--- src/clietsApp.ts
+++ src/clietsApp.ts
@@ -2,13 +2,14 @@ const button_Sign_in = document.querySelector(".button_Sign_in")!;
 const button_Sign_up = document.querySelector(".button_Sign_up")!;
 const FormSignIn = document.querySelector(".wrapper_form_Sign_in")!;
 const FormSignUp = document.querySelector(".wrapper_form_Sign_up")!;
-
+const button = document.querySelector(".button_submit")!;
+const form = document.forms!;
 let state = {
   classButton: "",
 };
 button_Sign_up.addEventListener("click", () => {
   state.classButton = "Sign_up";
-  console.log("Зарегестрироваться", state.classButton);
+  // console.log("Зарегестрироваться", state.classButton);
   changeSignUpAndSignIn();
 });
 button_Sign_in.addEventListener("click", () => {
@@ -16,7 +17,10 @@ button_Sign_in.addEventListener("click", () => {
   console.log("Войти", state.classButton);
   changeSignUpAndSignIn();
 });
-
+button.addEventListener("submit", (e: any) => {
+  e.preventDefault();
+  console.log(form.Form1.value);
+});
 function changeSignUpAndSignIn() {
   if (state.classButton === "Sign_up") {
     FormSignUp.classList.remove("display_flex");
